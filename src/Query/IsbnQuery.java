36c105e031d54f5487a4abf6c5379a48566e8bcb
Calvin <calvinwu4@gmail.com>
2017-03-18 04:45:38 -0400

    Simplified IsbnQuery

--- src/Query/IsbnQuery.java
+++ src/Query/IsbnQuery.java
@@ -1,6 +1,7 @@
 package Query;
 
 import Library.Book;
+import Library.Books;
 
 import java.util.ArrayList;
 
@@ -11,12 +12,7 @@ public class IsbnQuery implements BookQuery {
     @Override
     public ArrayList<Book> search(ArrayList<Book> books, String searchParam){
         ArrayList<Book> newBooks = new ArrayList<>();
-        books.get(Integer.parseInt(searchParam));
-        for(Book book: books){
-            if(book.getIsbn().equals(Integer.parseInt(searchParam))){
-                newBooks.add(book);
-            }
-        }
+        newBooks.add(Books.getBookHash().get(Integer.parseInt(searchParam)));
         return newBooks;
     }
 }
