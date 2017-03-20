44f61342b6f65fbeccbc0d3a1a79861b6aec86c6
Calvin <calvinwu4@gmail.com>
2017-03-20 01:25:52 -0400

    Deleted unnecessary functions in time Changed prints to return Strings

--- src/Library/Time.java
+++ src/Library/Time.java
@@ -17,11 +17,11 @@ public final class Time {
     /**
      * Helper function for incTime.
      */
-    public static void checkIsOpenAndClose(){
+    public static String checkIsOpenAndClose(){
         if(dateTime.isAfter(ChronoLocalDateTime.from(LocalTime.of(7,59))) &&
                 dateTime.isBefore(ChronoLocalDateTime.from(LocalTime.of(19,0)))){
             isOpen = true;
-            System.out.println("The library is open");
+            return("The library is open");
         }else {
             isOpen = false;
             for(Visitor visitor: Visitors.getVisitorHash().values()){
@@ -29,15 +29,16 @@ public final class Time {
                     visitor.getActiveVisit().setDeparture(LocalDateTime.now());
                 }
             }
-            System.out.println("The library has now closed");
+            return("The library has now closed");
         }
 
     }
+
     public static String incTime(Integer days, Integer hours) {
         if (days < 0 || days > 7) {
-            return ("Error (invalid number of days)");
+            return ("Invalid number of days entered.");
         } else if (hours < 0 || hours > 23 || days == 0 && hours == 0) {
-            return ("Error (invalid number of hours)");
+            return ("Invalid number of hours entered.");
         } else {
             dateTime.plusDays((days));
             dateTime.plusHours(hours);
@@ -48,23 +49,6 @@ public final class Time {
 
     public static String display(){
         return dateTime.format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm"));
-    }
-
-    //Setters (unneeded)
-    public static void setDate(String _date){
-        String [] dateArray = _date.split("/");
-        Integer year = Integer.parseInt(dateArray[2]);
-        Integer month = Integer.parseInt(dateArray[0]);
-        Integer day = Integer.parseInt(dateArray[1]);
-        dateTime = LocalDateTime.of(year, month, day, dateTime.getHour(), dateTime.getMinute());
-        // Catch parseInt and LocalDate.of exceptions
-    }
-    public static void setTime(String _time){
-        String [] timeArray = _time.split(":");
-        Integer hour = Integer.parseInt(timeArray[0]);
-        Integer minute = Integer.parseInt(timeArray[1]);
-        dateTime = LocalDateTime.of(dateTime.getYear(), dateTime.getMonth(), dateTime.getDayOfMonth(), hour, minute);
-        // Catch parseInt and LocalTime.of exceptions
     }
 
     // Getters
