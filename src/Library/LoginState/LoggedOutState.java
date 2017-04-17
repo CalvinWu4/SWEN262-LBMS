9ac96822b3801992535860704ca2d89bcf33cd21
Calvin <calvinwu4@gmail.com>
2017-04-16 20:19:58 -0400

    Put code into LoginState classes

--- src/Library/LoginState/LoggedOutState.java
+++ src/Library/LoginState/LoggedOutState.java
@@ -3,5 +3,9 @@ package Library.LoginState;
 /**
  * Created by Calvin on 4/11/2017.
  */
-public class LoggedOutState {
+public class LoggedOutState implements LoginState{
+    @Override
+    public void setContext(LoginContext context) {
+        context.setState(this);
+    }
 }
--- src/Library/LoginState/LoginState.java
+++ src/Library/LoginState/LoginState.java
@@ -4,4 +4,5 @@ package Library.LoginState;
  * Created by Calvin on 4/11/2017.
  */
 public interface LoginState {
+    void setContext(LoginContext context);
 }
